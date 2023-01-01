08847c31e8280653b7018758a10c4a66225ad260
penguin1109 <1109penguin@gmail.com>
2023-01-01 11:55:53 +0900

    [update] detection code + preprocessing

--- text_detection/detection.py
+++ text_detection/detection.py
@@ -5,6 +5,17 @@ import torch.nn as nn
 from torchvision import transforms
 import os, sys
 from loguru import logger
+import numpy as np
+STD = [0.20037157, 0.18366718, 0.19631825]
+MEAN = [0.90890862, 0.91631571, 0.90724233]
+BASE = '/content/drive/MyDrive/SpeakingFridgey/model_weights/detection'
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import os, sys
+from loguru import logger
 
 STD = [0.20037157, 0.18366718, 0.19631825]
 MEAN = [0.90890862, 0.91631571, 0.90724233]
@@ -15,7 +26,7 @@ def load_weight(weight_name, model):
   model_weight = model.state_dict()
   if 'model_state_dict' in pretrained:
     pretrained = pretrained['model_state_dict']
-  available = {key:value for (key, value) in pretrained_weight.items() if key in model_weight and \
+  available = {key:value for (key, value) in pretrained.items() if key in model_weight and \
                     value.shape == model_weight[key].shape}
   model_weight.update(available)
   model.load_state_dict(model_weight)
@@ -50,6 +61,7 @@ def make_divisable(value):
 
 def rescale_for_detect(image):
   C, H, W = image.shape
+  
   if H < W:
     new_w = 1024
     new_h = make_divisable(int(H * (1024 / W)))
@@ -59,6 +71,71 @@ def rescale_for_detect(image):
   rescale_factor = (W / new_w, H / new_h)
   logger.info(f"NEW_W: {new_w}, NEW_H: {new_h}")
   return (new_w, new_h), rescale_factor
+
+def preprocess(image):
+  gray_image = to_gray(image)
+  sobel, blurred = sobel_gradient(gray_image)
+  morpho = morphology(sobel,1, 1)
+  croped, points = crop(morpho, image)
+  return croped, points
+
+class DetectBot(object):
+  def __init__(self, remove_white=False):
+    self.crop = remove_white
+    self.cfg = CFG()
+    model = CTPN().cuda()
+    model = load_weight("CTPN_FINAL_CHECKPOINT.pth", model)
+    self.model = model
+    self.detector = TextDetector(self.cfg)
+
+  def predict(self, diff_h, diff_w, image: np.ndarray):
+    H, W, C = image.shape
+    original_image = image
+    if H > W:
+      new_shape = (2048, 1024) ## (H, W)
+    else:
+      new_shape = (1024, 2048)
+    rescale_factor = (W / new_shape[1], H / new_shape[0])
+    image = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize(new_shape),
+        transforms.Normalize(mean = MEAN, std = STD)
+      ])(image)
+    image = image.unsqueeze(0)
+
+    with torch.no_grad():
+      image = image.cuda()
+      reg, cls = self.model(image)
+
+    detected_boxes, scores = self.detector((reg, cls), image_size = new_shape)
+    print(len(detected_boxes))
+    ratio_w, ratio_h = rescale_factor
+    size_ = np.array([[ratio_w, ratio_h, ratio_w, ratio_h]])
+    detected_boxes *= size_
+    detected_boxes += np.array([[diff_w, diff_h, diff_w, diff_h]])
+    # detected_boxes = detected_boxes[rm_empty_box(original_image, detected_boxes)]
+
+    return detected_boxes
+
+  def __call__(self, image_path):
+    image = cv2.imread(image_path)
+    original_image = image
+    org_h, org_w, org_c = original_image.shape
+    if self.crop:
+      croped, points = preprocess(image)
+      print(len(points))
+    
+    else:
+      croped = [image];points = [(0, org_w, 0, org_h)];
+    all_box = []
+    for idx, croped_image in enumerate(croped):
+      croped_point = points[idx]
+      detected_boxes = self.predict(diff_h=croped_point[2], diff_w=croped_point[0], image=croped_image)
+      all_box.extend(detected_boxes)
+      original_image = draw_box(original_image, detected_boxes)
+    return original_image, all_box
+
+
 
 def detect(image_path):
   cfg = CFG()
